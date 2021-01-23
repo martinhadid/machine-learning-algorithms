8fc4d3287ec1e61a7bd7427beb17880fd0cc5abc
Martin Hadid <martin@safemode.co>
2021-01-23 13:14:21 +0200

    change parameter name to be consistent with other classes

--- logistic_regression_test.py
+++ logistic_regression_test.py
@@ -14,7 +14,7 @@ X, y = bc.data, bc.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-regressor = LogisticRegression(lr=0.0001, n_iters=1000)
+regressor = LogisticRegression(learning_rate=0.0001, n_iters=1000)
 regressor.fit(X_train, y_train)
 predictions = regressor.predict(X_test)
 
